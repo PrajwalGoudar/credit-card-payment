c9c4094523442cd0a5a34066065592188cb75c46
PrajwalGoudar <prajwaldgoudar@gmail.com>
2021-01-25 07:29:47 +0530

    bug fixes

--- src/app/common/validators/cvv.validators.ts
+++ src/app/common/validators/cvv.validators.ts
@@ -3,7 +3,7 @@ import { ValidationErrors } from '@angular/forms';
 export class CVVValidators{
 
     static isCVVInValid(control: AbstractControl) :ValidationErrors | null{
-        let cvvValidator = new RegExp(/^[0-9]{0,3}$/);
+        let cvvValidator = new RegExp(/^[0-9]{3}$/);
         if(cvvValidator.test(control.value as string) || (control.value as string).length===0){
              return null;
         }    
